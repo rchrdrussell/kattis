24f239f11a27f4928fdd9f5a5030c077bbace6fc
Richard Russell <9446579+RussellAjax@users.noreply.github.com>
2020-08-11 19:20:17 +0700

    11 august 2020

---
+++ 1.3/rtwo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+	int s, r;
+	scanf("%d %d", &r, &s);
+	printf("%d", 2*s-r);
+	return 0;
+}
---
+++ 1.3/solvingforcarrots.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int N;
+	int P;
+	scanf("%d %d", &N , &P);
+
+	printf("%d", P);
+	return 0;
+}
